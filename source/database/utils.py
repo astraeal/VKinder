1d92dbb0d925c6259bc417efad47581b306256d4
astraeal <astraeal@yandex.ru>
2023-06-29 20:59:14 +0300

    removed unused function database.utils.add_views_bulk

--- source/database/utils.py
+++ source/database/utils.py
@@ -8,13 +8,6 @@ def add_view(current_user_id: int, profile_user_id: int) -> None:
     )
 
 
-def add_views_bulk(current_user_id: int, profile_user_ids: list[int]) -> None:
-    Viewed.bulk_create([Viewed(
-        current_user_id=current_user_id,
-        profile_user_id=pui
-    ) for pui in profile_user_ids])
-
-
 def get_views(current_used_id: int) -> set[int]:
     result = Viewed.select(Viewed.profile_user_id) \
                 .where(Viewed.current_user_id == current_used_id) \
